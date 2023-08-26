57e3ae2a8067109099a0df41d6bf835a70a6acad
Adhnan Shereef T <93500202+adhnanshereef@users.noreply.github.com>
2023-08-26 10:18:33 +0000

    Managed Parameters tab also added queries in url, Header tab Managed and Prepared on the request, Cleaned the code of home.component.ts, add fontawesome cdn

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,46 +1,172 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+// Interface for parameters
+interface Parameter {
+  key: string;
+  value: string;
+}
+
+// Component decorator
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  requestForm = this.formBuilder.group({ type: 'POST', url: '', body: '{}' });
-  response: any = { "message": "Send a request!" };
+
+// Component class
+export class HomeComponent implements OnInit {
+  // Variables
+  response: any = { message: 'Send a request!' };
   index: string = 'bORp';
-  
+  requestForm: FormGroup = new FormGroup({});
+
   constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
-  
+
+  // Function to initialize the form
+  ngOnInit() {
+    // Initializing the form
+    this.requestForm = this.formBuilder.group({
+      type: 'GET',
+      url: '',
+      body: '{}',
+      parameters: this.formBuilder.array([]), // Initialize parameters array
+      headers: this.formBuilder.array([]), // Initialize headers array
+    });
+
+    // Adding a default parameter
+    this.addParameter();
+    // Adding a default header
+    this.addHeader();
+  }
+
+  // Getter for parameters
+  get parameters() {
+    return this.requestForm.get('parameters') as FormArray;
+  }
+
+  // Function to add a parameter
+  addParameter() {
+    const parameterGroup = this.formBuilder.group({
+      key: '',
+      value: '',
+    });
+    this.parameters.push(parameterGroup);
+  }
+
+  // Function to remove a parameter
+  removeParameter(index: number) {
+    this.parameters.removeAt(index);
+  }
+
+  // Function to delete all parameters
+  deleteAllParameters() {
+    this.parameters.clear();
+  }
+
+  // Getter for headers
+  get headers() {
+    return this.requestForm.get('headers') as FormArray;
+  }
+
+  // Function to add a header
+  addHeader() {
+    const headerGroup = this.formBuilder.group({
+      key: '',
+      value: '',
+    });
+    this.headers.push(headerGroup);
+  }
+
+  // Function to remove a header
+  removeHeader(index: number) {
+    this.headers.removeAt(index);
+  }
+
+  // Function to delete all headers
+  deleteAllHeaders() {
+    this.headers.clear();
+  }
+
+  checkForm() {
+    const formData = this.requestForm.value;
+    console.log('Form Data:', formData);
+
+    // You can access the parameters like this:
+    const parameters = formData.parameters;
+    console.log('Parameters:', parameters);
+
+    const headers = formData.headers;
+    console.log('Headers:', headers);
+
+    // Iterate through parameters and access key and value:
+    for (const param of parameters) {
+      console.log('Parameter:', param.key, 'Value:', param.value);
+    }
+
+    // Iterate through headers and access key and value:
+    for (const header of headers) {
+      console.log('Header:', header.key, 'Value:', header.value);
+    }
+
+    // Now you can use the formData to make your request or perform other actions
+  }
+
+  // To navigate among the tabs
   setIndex(index: string) {
     this.index = index;
   }
 
+  // Function to adjust the height of the textarea
   adjustTextareaHeight(event: any): void {
     const textarea = event.target as HTMLTextAreaElement;
     if (textarea) {
       textarea.style.height = 'auto';
-      textarea.style.height = (textarea.scrollHeight) + 'px';
+      textarea.style.height = textarea.scrollHeight + 'px';
     }
   }
 
-
+  // Function to send the request
   onSendRequest() {
+    // Setting the response to loading
     this.response = 'Loading...';
-    let { type, url, body } = this.requestForm.value;
-    url = url?.toString() || '';
+
+    // Getting the form values
+    let { type, url, body, parameters } = this.requestForm.value;
+
+    // Preparing the headers
+    const headers = this.prepareHeaders();
+
+    // Checking for empty URL
+    if (!url) {
+      this.response = { alert: 'URL is required.' };
+      return;
+    }
+
+    // Checking for valid URL
     try {
       new URL(url);
     } catch (error) {
       this.response = 'Invalid URL';
       return;
     }
-    body = body?.toString() || '{}';
 
+    // Calling the API
     if (type === 'GET') {
-      this.http.get(url).subscribe(
+      // Checking for empty parameters
+      const validParameters = parameters.filter(
+        (param: Parameter) => param.key.trim() !== ''
+      );
+      // Constructing the full URL with parameters
+      const fullUrl =
+        validParameters.length > 0
+          ? `${url}?${validParameters
+              .map((param: Parameter) => `${param.key}=${param.value}`)
+              .join('&')}`
+          : url;
+      // Sending the GET request
+      this.http.get(fullUrl, { headers }).subscribe(
         (res) => {
           this.response = res;
         },
@@ -48,14 +174,18 @@ export class HomeComponent {
           this.response = error;
         }
       );
+
+      // Completed the GET request
     } else if (type === 'POST') {
+      // Checking whether the body is valid JSON
       try {
         body = JSON.parse(body);
       } catch (error) {
         this.response = 'Invalid JSON body!';
         return;
       }
-      this.http.post(url, body).subscribe(
+      // Sending the POST request
+      this.http.post(url, body, { headers }).subscribe(
         (res) => {
           this.response = res;
         },
@@ -63,8 +193,35 @@ export class HomeComponent {
           this.response = error;
         }
       );
+      // Completed the POST request
     } else {
+      // Invalid request type
       this.response = 'Invalid request type!';
     }
+    // End of API call
   }
+  // End of onSendRequest()
+
+  // Helper function to prepare headers from the form
+  prepareHeaders(): HttpHeaders {
+    const headerControls = this.headers.controls;
+
+    const headers = new HttpHeaders();
+    headerControls.forEach((control) => {
+      const keyControl = control.get('key');
+      const valueControl = control.get('value');
+
+      if (keyControl && valueControl) {
+        const key = keyControl.value.trim();
+        const value = valueControl.value.trim();
+        if (key !== '') {
+          // Check if the key is not empty
+          headers.append(key, value);
+        }
+      }
+    });
+
+    return headers;
+  }
+  // End of prepareHeaders()
 }
